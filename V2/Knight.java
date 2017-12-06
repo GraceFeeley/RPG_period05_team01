7bb7217732f90690ab04d89a785a77e6b0df41d7
GraceFeeley <gfeeley@stuy.edu>
2017-12-05 19:32:09 -0500

    Knight.java

--- V2/Knight.java
+++ V2/Knight.java
@@ -4,12 +4,17 @@ public class Knight extends Protagonist{
     public Knight(String s, int swordStrength){
 	name = s;
 	health = 400;
-	strength = 70;\
+	strength = 70;
 	defense = 55;
 	rating = .9;
 	sword = swordStrength;
     }
 
-    public void Slain(){
-    }}
+    public void Slain(Monster raul){
+	health = (strength * sword * rating)-raul.defense;
+    }
+
+    public string toString(){
+	return "If thou doth dares, you can go up against this knight, "+name+". It has the health of "+health+" and the strength of "+strength+". Watch out for his sword!";
+}
 	
