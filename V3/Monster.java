663c831ce62ca125389d368d3b13f1598f0396f0
Fareeha09 <ftabassum@stuy.edu>
2017-12-14 20:25:03 -0500

    Monster.java

--- V3/Monster.java
+++ V3/Monster.java
@@ -4,7 +4,7 @@ public class Monster extends Character {
         strength = (int)(20 + 40 * Math.random());
         defense = 40;
         rating = 0.3;
-    }
+     }
     public String toString(){
 	return "Ahh run away from the monster! It has a health of "+health+", a strength of "+strength+", a defense of "+defense+", and a rating of "+rating+".";
 }
